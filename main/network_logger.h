4723f8ecffad089ef798b3fb25ae33934ac087a7
Omar <omarfaruk.dc@gmail.com>
2025-09-08 22:19:11 +0600

    wifi network changed

--- main/network_logger.h
+++ main/network_logger.h
@@ -1,6 +1,6 @@
 #define SERVER_PORT 9999
 #define SERVER_IP   "192.168.0.181"
-#define WIFI_SSID   "Home"
-#define WIFI_PASS   "home@694/3.wk"
+#define WIFI_SSID   "Home AP"
+#define WIFI_PASS   "123456789"
 
 void network_logging_init();
